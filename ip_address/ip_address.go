371cdb6df2f0d50c623899b1b5953df73d130459
mfaishal882 <mfaishal882@gmail.com>
2023-01-24 02:51:21 +0700

    ip address

---
+++ ip_address/ip_address.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func defangIPaddr(address string) string {
+	//return strings.Replace(address, ".", "[.]", -1)
+
+	// replace using byte
+	var result []byte
+	for _, value := range address {
+		if value == '.' {
+			result = append(result, "[.]"...)
+		} else {
+			result = append(result, byte(value))
+		}
+	}
+	return string(result)
+}
+
+func main() {
+	fmt.Println(defangIPaddr("1.1.1.1"))
+}
